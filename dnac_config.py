464ac35f4a4ab5dc07a0f6d6b4cf5ce79a6bd214
Nelson Tovar <ntovar@fiec.espol.edu.ec>
2025-04-03 18:26:29 -0700

    Succesfully Done

---
+++ dnac_config.py
@@ -0,0 +1,11 @@
+
+DNAC = {
+    "host": "sandboxdnac.cisco.com",
+    "port": 443,
+    "username": "devnetuser",
+    "password": "Cisco123!"
+}
+
+# Note: Credentials can be gotten from the DevNet Sandbox page.
+# https://developer.cisco.com/sandbox/
+# I picked 'Catalyst Center Always-On v2.3.3.6' for this example.
